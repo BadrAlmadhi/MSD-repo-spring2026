9a0d43c1f943c0c9483e097ec47cd3ade5d1f383
MSD <msdadmin@msds-air.uconnect.utah.edu>
2026-04-09 19:15:56 -0600

    fixed the bug, now ./msdscript --interp, print, pretty-print should work

--- cs6015/week3/projectphase2/main.cpp
+++ cs6015/week3/projectphase2/main.cpp
@@ -14,27 +14,39 @@ int main(int argc, char **argv) {
         run_mode_t mode = use_arguments(argc, argv);
 
         switch (mode) {
-        
             case do_test: {
             std::cerr << "Run tests using the test executable\n";
             return 1;
         }
 
+        // my code used to work only when using echo 
+        // fixed bug
         case do_interp: {
-            PTR(Expr) e = parse(std::cin);
-            std::cout << e->interp(Env::empty)->to_string() << "\n";
+            // we need to make sure C++ know we are done typing
+            // using echo immediately sends EOF tells C++ line if finished
+            std::string input;
+            std::getline(std::cin, input); // C++ now reads only one line, after this line input is done
+
+            PTR(Expr) e = parse_str(input); // convert stream input into string
+            std::cout << e->interp(Env::empty)->to_string() << "\n"; // then interp
             return 0;
         }
 
         case do_print: {
-            PTR(Expr) e = parse(std::cin);
+            std::string input;
+            std::getline(std::cin, input);
+
+            PTR(Expr) e = parse_str(input);
             std::cout << e->to_string() << "\n";
             return 0;
         }
 
         case do_pretty_print: {
             // Parse expression from stdin, then pretty print it.
-            PTR(Expr) e = parse(std::cin);
+            std::string input;
+            std::getline(std::cin, input);
+
+            PTR(Expr) e = parse_str(input);
             std::cout << e->to_pretty_string() << "\n";
             return 0;
         }
